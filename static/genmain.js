fa48fb785295dfe2a3a655e5f11e689810f98f52
zahle <zahlekhaned@gmail.com>
2017-12-28 21:09:10 +0530

    eti fix

--- static/genmain.js
+++ static/genmain.js
@@ -20,7 +20,7 @@ as:[1,1,1,1,1]
 {
 parentEvent: 'supernova',
 events:['scientists-utopia', 'astrophotography', 'astroquiz', 'exploring-interstellar'],
-max:[3,1,1,2],
+max:[3,1,1,3],
 as:[1,1,1,1]
 },
 {
