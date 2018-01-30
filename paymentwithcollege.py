82b1d601f42c99f37540885ad57210b19bed947b
kushagra-97 <kushagras96@gmail.com>
2018-01-30 23:39:57 +0530

    sheetpayment

--- paymentwithcollege.py
+++ paymentwithcollege.py
@@ -8,7 +8,8 @@ from Auth.views import *
 
 
 pay = sheetpayment.objects.all()
-for pro in pay:
+p=pay[0]
+for pro in p:
     print(pro)
     dic = {
         "name" : pro.tech.user.first_name,
@@ -21,6 +22,6 @@ for pro in pay:
         "registeredOn" : pro.timeStamp,
         }
 
-    url = "https://script.google.com/a/technex.in/macros/s/AKfycbxX16VxFckPcIaZ5sr0Yjmy9-LPymm8nwXOhMN62aLbirltmt3i/exec" #tech@technex.in
-    #url='https://script.google.com/a/technex.in/macros/s/AKfycbykHL9khnVUO0cM_pQ8W7MJ-avy_K8Go8d0K21HRlLFsgR1CrI/exec' #events@technex.in
-    print(requests.post(url,data=dic))
+    # url = "https://script.google.com/a/technex.in/macros/s/AKfycbxX16VxFckPcIaZ5sr0Yjmy9-LPymm8nwXOhMN62aLbirltmt3i/exec" #tech@technex.in
+    # #url='https://script.google.com/a/technex.in/macros/s/AKfycbykHL9khnVUO0cM_pQ8W7MJ-avy_K8Go8d0K21HRlLFsgR1CrI/exec' #events@technex.in
+    # print(requests.post(url,data=dic))
