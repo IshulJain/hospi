ea4753092e7ec4fa12bc73c256ce9fad6306141f
kushagra-97 <kushagras96@gmail.com>
2018-01-30 19:10:54 +0530

    contacts

--- eventws.py
+++ eventws.py
@@ -4,34 +4,50 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technex17.settings")
 application = get_wsgi_application()
 from Auth.models import * 
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from Auth.views import *
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
 
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+# # use creds to create a client to interact with the Google Drive API
+# scope = ['https://spreadsheets.google.com/feeds']
+# creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+# client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("ddetails").sheet1
+# # Find a workbook by name and open the first sheet
+# # Make sure you use the right name here.
+# sheet = client.open("ddetails").sheet1
 
 teams=Team.objects.all()
 members=[]
 for t in teams:
-	m=t.members
-	members.append(m)
+	m=t.members.all()
+	leader=t.teamLeader
+	for mem in m:
+		members.append(mem)
+	
+	members.append(leader)
+	print(members)
+	
+	
 
 work=WorkshopTeam.objects.all()
 final=[]
 for member in members:
 	for w in work:
-		if member.email!=w.teamName:
+		if member.technexId!=w.teamLeader.technexId:
 			final.append(member)
 
-w = Workbook()
-m = 0
-sheet1 = w.add_sheet('Sheet1')
-
+dic = {
+        "name" : techprofile.user.first_name,
+        "email" : techprofile.email,
+        "college" : techprofile.college.collegeName,
+        "technexId" : techprofile.technexId,
+        "year" : techprofile.year,
+        "mobileNumber" : techprofile.mobileNumber,
+        "city" : techprofile.city
+        }
+url = "https://script.google.com/a/technex.in/macros/s/AKfycbwIXDuKjAipVNAWj8cjVAQrurLg7nWLU1s7nDCZD41yhSucG4I/exec" #tech@technex.in
+    #url='https://script.google.com/a/technex.in/macros/s/AKfycbykHL9khnVUO0cM_pQ8W7MJ-avy_K8Go8d0K21HRlLFsgR1CrI/exec' #events@technex.in
+    requests.post(url,data=dic)
 
