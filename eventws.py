f7f5b7cbd3b4fa6f432dfb054c3c747f2080d4e2
kushagra-97 <kushagras96@gmail.com>
2018-01-30 21:04:41 +0530

    events

--- eventws.py
+++ eventws.py
@@ -52,7 +52,7 @@ for mem in final:
 	        "city" : mem.city
 	        }
 
-	url = "https://script.google.com/a/technex.in/macros/s/AKfycbwIXDuKjAipVNAWj8cjVAQrurLg7nWLU1s7nDCZD41yhSucG4I/exec" #tech@technex.in
+	url = "https://script.google.com/a/technex.in/macros/s/AKfycby347_r4AzmOqWdz2merV-ibi7umjzmeUuWiMWhPvAE_9k-WZVa/exec" #tech@technex.in
 	    #url='https://script.google.com/a/technex.in/macros/s/AKfycbykHL9khnVUO0cM_pQ8W7MJ-avy_K8Go8d0K21HRlLFsgR1CrI/exec' #events@technex.in
 	    requests.post(url,data=dic)
 
