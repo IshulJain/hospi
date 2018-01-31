59cfdef8da3faba5461ccb6821dba7139e1b922f
kushagra-97 <kushagras96@gmail.com>
2018-01-31 15:36:50 +0530

    regwithoutpay

--- regwithoutpay.py
+++ regwithoutpay.py
@@ -53,21 +53,32 @@ for t in techp:
 
 	if c==0:
 		final.append(t)
+
 for mem in final:
+	try:
+		sheetpayment.objects.get(tech = mem)
+		print(mem)
+		final.remove(mem)
+	except:
+		pass
 
-	dic = {
-	        "name" : mem.user.first_name,
-	        "email" : mem.email,
-	        "college" : mem.college.collegeName,
-	        "technexId" : mem.technexId,
-	        "year" : mem.year,
-	        "mobileNumber" : mem.mobileNumber,
-	        "city" : mem.city
-	        }
 
-	url = "https://script.google.com/a/technex.in/macros/s/AKfycbwgg6rfk29Yhenn4zSNqc_2lSrciVMM3ixqbpS_9cQkSNvPzl0/exec" #tech@technex.in
-	    #url='https://script.google.com/a/technex.in/macros/s/AKfycbykHL9khnVUO0cM_pQ8W7MJ-avy_K8Go8d0K21HRlLFsgR1CrI/exec' #events@technex.in
-	print(requests.post(url,data=dic))
+
+# for mem in final:
+
+# 	dic = {
+# 	        "name" : mem.user.first_name,
+# 	        "email" : mem.email,
+# 	        "college" : mem.college.collegeName,
+# 	        "technexId" : mem.technexId,
+# 	        "year" : mem.year,
+# 	        "mobileNumber" : mem.mobileNumber,
+# 	        "city" : mem.city
+# 	        }
+
+# 	url = "https://script.google.com/a/technex.in/macros/s/AKfycbwgg6rfk29Yhenn4zSNqc_2lSrciVMM3ixqbpS_9cQkSNvPzl0/exec" #tech@technex.in
+# 	    #url='https://script.google.com/a/technex.in/macros/s/AKfycbykHL9khnVUO0cM_pQ8W7MJ-avy_K8Go8d0K21HRlLFsgR1CrI/exec' #events@technex.in
+# 	print(requests.post(url,data=dic))
 
 
 
