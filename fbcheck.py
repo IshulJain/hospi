92ff8c874a9b6f9b096d7dd8dac4d83438d554ce
Mr-K10 <kautilyakatiha10@gmail.com>
2018-02-02 19:16:41 +0530

    fbcheck

--- fbcheck.py
+++ fbcheck.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
+import requests
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "technex17.settings")
 application = get_wsgi_application()
