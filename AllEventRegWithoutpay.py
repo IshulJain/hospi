01fb941898a0b0ab0a6c75e3c839bab9a9beefc6
kushagra-97 <kushagras96@gmail.com>
2018-02-04 19:40:53 +0530

    all event list without pay

--- AllEventRegWithoutpay.py
+++ AllEventRegWithoutpay.py
@@ -6,16 +6,49 @@ from Auth.models import *
 from Auth.views import *
 
 teams=Team.objects.all()
-members=[]
+
 for team in teams:
+	members=[]
 	team_members=team.members.all()
+	event=team.event
 	# print(m)
 	leader=team.teamLeader
 	# print(leader)
 	for mem in team_members:
 		members.append(mem)
 	members.append(leader)
-memberss=list(set(members))
+
+	for member in members:
+		final=[]
+		pays = sheetpayment.objects.filter(tech = member)
+		if len(pays) == 0 and not member.college.collegeWebsite == "190":
+			final.append(member)	
+			print member.user.first_name
+		if len(final) != 0:
+			print(member.email)
+			print(member.event)
+
+
+
+
+			# for mem in final:
+			# 	dic = {
+	  #       "name" : mem.user.first_name,
+	  #       "email" : mem.email,
+	  #       "college" : mem.college.collegeName,
+	  #       "technexId" : mem.technexId,
+	  #       "year" : mem.year,
+	  #       "mobileNumber" : mem.mobileNumber,
+	  #       "city" : mem.city,
+	  #       "events":event
+	  #       }
+
+			# url = "https://script.google.com/a/technex.in/macros/s/AKfycbwx8RQasAIvjEIy-ewy9maEgdMWYWxiKCoD4GE0rX_RgUf1vFHb/exec" #tech@technex.in
+			    #url='https://script.google.com/a/technex.in/macros/s/AKfycbykHL9khnVUO0cM_pQ8W7MJ-avy_K8Go8d0K21HRlLFsgR1CrI/exec' #events@technex.in
+			# print(requests.post(url,data=dic))
+
+
+# memberss=list(set(members))
 
 # workmem=[]
 # for workshopteam in workshopteams:
@@ -27,18 +60,18 @@ memberss=list(set(members))
 
 
 # no_reg = [x for x in techp if x not in total_reg]
-final=[]
-for member in memberss:
-	pays = sheetpayment.objects.filter(tech = member)
-	print pays
-	if len(pays) == 0 and not member.college.collegeWebsite == "190":
-		final.append(member)	
-		print member.user.first_name
+# final=[]
+# for member in memberss:
+# 	pays = sheetpayment.objects.filter(tech = member)
+# 	# print pays
+# 	if len(pays) == 0 and not member.college.collegeWebsite == "190":
+# 		final.append(member)	
+# 		print member.user.first_name
 
-for mem in final:
-	events=mem.event
-	print events
-	print("next")
+# for mem in final:
+# 	events=mem.event
+# 	print events
+# 	print("next")
 
 	# dic = {
 	#         "name" : mem.user.first_name,
